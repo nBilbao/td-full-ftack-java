6fa9522cd0f78ff90107d495041b32badd983903
nBilbaoe <nbilbao.s@gmail.com>
2021-05-01 14:38:07 -0400

    ninja desapareciendo

---
+++ Ninja Desapareciendo/main.js
@@ -0,0 +1,30 @@
+$("#1").click(function(){
+    $("#1").fadeOut("slow");
+})
+$("#2").click(function(){
+    $("#2").fadeOut("slow");
+})
+$("#3").click(function(){
+    $("#3").fadeOut("slow");
+})
+$("#4").click(function(){
+    $("#4").fadeOut("slow");
+})
+$("#5").click(function(){
+    $("#5").fadeOut("slow");
+})
+$("#6").click(function(){
+    $("#6").fadeOut("slow");
+})
+$("#7").click(function(){
+    $("#7").fadeOut("slow");
+})
+$("#8").click(function(){
+    $("#8").fadeOut("slow");
+})
+
+
+
+$("button").click(function(){
+    $("div:hidden").fadeIn("slow");
+})
