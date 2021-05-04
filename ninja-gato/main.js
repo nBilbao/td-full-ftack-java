78e2542c216b31894d52581db068c47e6c3b384e
nBilbaoe <nbilbao.s@gmail.com>
2021-05-03 21:44:01 -0400

    Se cambian de imagenes cambiando el atributo src por el data-ald-src

--- ninja-gato/main.js
+++ ninja-gato/main.js
@@ -1,12 +1,43 @@
+$('#imagen0').click(function() {
+  
+  if($("#imagen0").attr("src")=="img/ninja0.png"){
+    $("#imagen0").attr("src","img/cat0.png");
+  }
+  else{
+    $("#imagen0").attr("src","img/ninja0.png");
+  }
+});
 
-
-$('#img1').click(function() {
-    $("#img1").fadeOut("slow")
-    //$('src', $(this).attr('data-alt-src'));
-    $('#img1').attr('src','img/cat1.png');
-    $("#img1").fadeIn("slow")
-  });
+$('#imagen1').click(function() {
+  if($("#imagen1").attr("src")=="img/ninja1.png"){
+    $("#imagen1").attr("src","img/cat1.png");
+  }
+  else{
+    $("#imagen1").attr("src","img/ninja1.png");
+  }
+});
   
-  
-  
-  
+$('#imagen2').click(function() {
+  if($("#imagen2").attr("src")=="img/ninja2.png"){
+    $("#imagen2").attr("src","img/cat2.png");
+  }
+  else{
+    $("#imagen2").attr("src","img/ninja2.png");
+  }
+});
+$('#imagen3').click(function() {
+  if($("#imagen3").attr("src")=="img/ninja3.png"){
+    $("#imagen3").attr("src","img/cat3.png");
+  }
+  else{
+    $("#imagen3").attr("src","img/ninja3.png");
+  }
+});
+$('#imagen4').click(function() {
+  if($("#imagen4").attr("src")=="img/ninja4.png"){
+    $("#imagen4").attr("src","img/cat4.png");
+  }
+  else{
+    $("#imagen4").attr("src","img/ninja4.png");
+  }
+});
