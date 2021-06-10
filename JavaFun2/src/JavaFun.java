793464bb7d31acad938ab35f759ecdc550774faa
nBilbaoe <nbilbao.s@gmail.com>
2021-06-10 18:55:00 -0400

    Ejercicio java fun de coding Dojo

---
+++ JavaFun2/src/JavaFun.java
@@ -0,0 +1,12 @@
+public class JavaFun {
+    public static void main(String[] args) {
+        System.out.println(datos());
+
+    }
+    public static String datos(){
+        String nombre="Nombre: Nemesio Bilbao Salazar.\n" +
+                "Edad: 34 años.\n" +
+                "Ciudad: Loncoche IX Región.\n";
+        return nombre;
+    }
+}
