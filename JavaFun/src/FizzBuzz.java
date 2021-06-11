e22ade956a25c82c585aba5bdf4c60fae6cb8fe1
nBilbaoe <nbilbao.s@gmail.com>
2021-06-10 21:11:31 -0400

    Separación en 2 clases de FizzBuzz

--- JavaFun/src/FizzBuzz.java
+++ JavaFun/src/FizzBuzz.java
@@ -1,13 +1,5 @@
 public class FizzBuzz {
-    public static void main(String[] args) {
-
-        System.out.println(fizzBuzz(3));
-        System.out.println(fizzBuzz(5));
-        System.out.println(fizzBuzz(15));
-        System.out.println(fizzBuzz(2));
-
-    }
-    public static String fizzBuzz(int number){
+    public String fizzBuzz(int number){
         String salida="";
         if(number%3==0 && number%5==0){
             salida="FizzBuzz";
