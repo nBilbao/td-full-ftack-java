4065676c7f12dbb39c33585d8b42086ac8a8fb8b
nBilbao <nbilbao.s@gmail.com>
2021-08-18 19:43:26 -0400

    employee

---
+++ EmpleadosGerentesSpring/src/main/java/com/nbilbao/EmpleadosGerentesSpring/EmpleadosGerentesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nbilbao.EmpleadosGerentesSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleadosGerentesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleadosGerentesSpringApplication.class, args);
+	}
+
+}
---
+++ EmpleadosGerentesSpring/src/test/java/com/nbilbao/EmpleadosGerentesSpring/EmpleadosGerentesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbilbao.EmpleadosGerentesSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleadosGerentesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
